f5290e57af472388702e6a88ca45ac1b4f0f35eb
wahek <wahek1999@mail.ru>
2024-03-12 08:44:06 +0300

    registration/authenticated done

--- RecipeBook/settings.py
+++ RecipeBook/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'recipebook.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'ivanpydev.pythonanywhere.com']
 
 # Application definition
 
