72329e51b64859cd07de985100c047bf7a3f3a9c
wahek <wahek1999@mail.ru>
2024-03-12 08:46:40 +0300

    registration/authenticated done

--- RecipeBook/settings.py
+++ RecipeBook/settings.py
@@ -127,8 +127,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_ROOT = [os.path.join(BASE_DIR, 'media/')]
 MEDIA_URL = 'media/'
