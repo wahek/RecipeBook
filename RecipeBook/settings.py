e47cce7d8f9054c8e26a6f0d0f9ecf8d479318bc
wahek <wahek1999@mail.ru>
2024-03-12 08:23:56 +0300

    registration/authenticated done

--- RecipeBook/settings.py
+++ RecipeBook/settings.py
@@ -14,11 +14,9 @@ import os.path
 from pathlib import Path
 from dotenv import load_dotenv
 
-load_dotenv()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
